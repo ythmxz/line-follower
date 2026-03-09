33c2c1ca29d4c3575b50b5901e40347d3e1f321e
Yuri Thomaz <ytcarvalho14@gmail.com>
2026-03-09 00:15:45 -0300

    Adiciona controle de velocidade

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.ev3devices import (Motor, ColorSensor)
+from pybricks.parameters import Port, Button, Color
+from pybricks.tools import wait
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
+# Objects
 ev3 = EV3Brick()
 motor_left = Motor(Port.B)
 motor_right = Motor(Port.C)
 color_sensor = ColorSensor(Port.S1)
 
-SPEED = 200.0
+# Variables
+speed = 200
 
-# Write your program here.
-while not ev3.buttons.pressed():
+# Program
+while not Button.CENTER in ev3.buttons.pressed():
+    if Button.UP in ev3.buttons.pressed():
+        speed += 10
+
+    elif Button.DOWN in ev3.buttons.pressed():
+        speed -= 10
+
     if color_sensor.color() == Color.BLACK:
-        motor_left.run(SPEED)
-        motor_right.run(SPEED * 0.5)
+        motor_left.run(speed)
+        motor_right.run(speed // 4)
+        ev3.screen.print("RIGHT")
+
     else:
-        motor_left.run(SPEED * 0.5)
-        motor_right.run(SPEED)
+        motor_left.run(speed // 4)
+        motor_right.run(speed)
+        ev3.screen.print("LEFT")
 
     wait(10)
 
